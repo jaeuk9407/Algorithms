aae4343796c1e4f0f28dc512af534865cacd9a01
Jaeuk <46706670+jaeuk9407@users.noreply.github.com>
2020-03-05 10:27:24 +0900

    Add files via upload

---
+++ N2741.java
@@ -0,0 +1,17 @@
+package io;
+
+import java.util.Scanner;
+
+public class N2741 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i=1; i<n+1; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
