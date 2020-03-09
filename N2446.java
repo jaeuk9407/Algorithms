413af77aa8f86c84066aa94ec3fc9910e6676f37
Jaeuk <46706670+jaeuk9407@users.noreply.github.com>
2020-03-09 23:08:34 +0900

    Add files via upload

---
+++ N2446.java
@@ -0,0 +1,37 @@
+package io;
+
+import java.util.Scanner;
+
+public class N2446 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+		
+		for(int p = 0; p<n; p++) {
+			for(int q=0; q<p;q++) {
+				System.out.print(" ");
+			}
+			for(int q=0; q<2*(n-p)-1;q++) {
+				System.out.print("*");
+			}
+
+			System.out.println("");
+		}
+		
+		for(int p = 1; p<n; p++) {
+			for(int q=0; q<n-p-1;q++) {
+				System.out.print(" ");
+			}
+			for(int q=0; q<2*p+1;q++) {
+				System.out.print("*");
+			}
+
+			System.out.println("");
+		}
+		
+	}
+
+}
