d67f1b166319289ca3544619e88e559a90175c4b
socialDe <jaeuk9407@gmail.com>
2021-02-10 11:25:05 +0900

    Update P1208 Solutions_switching

--- BOJ/src/exhaustiveSearch/P1208_2.java
+++ BOJ/src/exhaustiveSearch/P1208_2.java
@@ -1,106 +1,92 @@
 package exhaustiveSearch;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.StringTokenizer;
 
-// Wrong Answer
+// 참고 https://kora1492.tistory.com/22
 
 public class P1208_2 {
-	static int N, S, cnt;
-	static ArrayList<Integer> leftList = new ArrayList<>();
-	static ArrayList<Integer> rightList = new ArrayList<>();
-	static int[] arr;
-	public static void main(String[] args) throws Exception{
+	static ArrayList<Integer> A,B;
+	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		S = Integer.parseInt(st.nextToken());
-		
-		arr = new int[N];
-		
+		int N = Integer.parseInt(st.nextToken());
+		int S = Integer.parseInt(st.nextToken());
+		int [] arr = new int [N];
 		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < N; i++) {
+		for(int i=0;i<N;i++){
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
+		A = new ArrayList<Integer>();
+		B = new ArrayList<Integer>();
 		
-		// 입력받은 배열을 두 부분으로 나눠 각 부분에서 모든 부분 수열의 합 case들을 list에 저장  
-		makeSum(0, 0, N/2, leftList);
-		makeSum(0, N/2, N, rightList);
+		// 40까지의 합을 구하는 것은 범위가 커서 시간초과 발생
+		// 절반으로 나눠서 A(0~N/2), B(N/2~N)로 부분수열의 합을 구함.
+		dfs(arr, 0, N/2, 0, A);
+		dfs(arr, N/2,N,  0, B);
 		
-		// 모든 부분 수열의 합 case가 담긴 list를 오름차순으로 정렬
-		Collections.sort(leftList);
-		Collections.sort(rightList);
-//		System.out.println(leftList.toString());
-//		System.out.println(rightList.toString());
-//		System.out.println("----------------------------------------------");
+		// 정렬
+		Collections.sort(A);
+		Collections.sort(B);
 		
-		cnt = 0;
-		calcC();
+		// left, right 를 lower_bound, upper_bound를 잡아서 한다.
+		// left, right 반대로 해도 상관없었다. 
 		
-		// 합이 0인 수를 찾는 경우, 부분 수열의 합을 찾을 때 하나도 선택하지 않은 경우가 포함되기 때문에 한 번 빼주어야 함
-		if(S == 0) {
-			System.out.println(cnt - 1);
-		}else {
-			System.out.println(cnt);
-		}
-		
-		
-	}// end of main
-	
-	private static void calcC() {
-		int pointerL = 0;
-		int pointerR = rightList.size()-1;
-		
-		while(true) {
-			if(pointerL == leftList.size() || pointerR < 0) {
-				break;
-			}
-			int lv = leftList.get(pointerL);
-			int rv = rightList.get(pointerR);
-			
-			// 합이 목적 값과 같으면 합을 이루고 있는 각 수가 list 내에 몇개 있는지 센다. 
-			if(lv + rv == S) {
-				int lc = 0;
-				while(pointerL < leftList.size() && leftList.get(pointerL) == lv) {
+		int left = 0;
+		int right = B.size()-1;
+		long ans=0;
+		while(left<A.size() && right>=0){
+			int lv = A.get(left);
+			int rv = B.get(right);
+			if(lv+rv==S){
+				// lv + rv 의 합이 S일 때 처리
+				long lc = 0;
+				long rc = 0;
+				// 왼쪽에서 중복되는 값 개수를 카운팅한다(자기포함).
+				while(left<A.size() && A.get(left)==lv){
 					lc++;
-					pointerL++;
+					left++;
 				}
-				
-				int rc = 0;
-				while(0 <= pointerR && rightList.get(pointerR) == rv) {
+				// 오른쪽에서 중복되는 값 개수를 카운팅한다(자기포함).
+				while(right>=0 && B.get(right)==rv){
 					rc++;
-					pointerR--;
+					right--;
 				}
-				cnt += lc * rc;
+				// 개수를 곱해준다. 해당되는 경우의 수를 구하는 문제이기 때문에
+				ans += lc*rc;
 			}
-			
-			// 목적하는 값보다 더 큰 경우
-			if(lv + rv > S) {
-				pointerR--;
+			// 구하고자 합보다 큰 경우 right를 뺀다.
+			if(lv+rv>S){
+				right--;
 			}
-			
-			// 목적하는 값보다 더 작은 경우 
-			if(lv + rv < S) {
-				pointerL++;
+			// 구하고자 하는 합보다 작은 경우 low를 더한다.
+			if(lv+rv <S) {
+				left++;
 			}
-			
-			
+		}
+		// 구하고자 하는 합이 0일 때, 공집합(아무것도 선택하지 않은 값=0)으로 되어 있어서 답에서 -1를 해준다.
+		if(S==0){
+			System.out.println(ans-1);
+		}else {
+			System.out.println(ans);
+		}
+	}
+	// 부분수열의 합을 구하는 재귀함수
+	public static void dfs(int arr[], int idx, int n, int sum, ArrayList<Integer> list){
+		
+		if(idx==n){
+			list.add(sum);
+			return ;
 		}
 		
+		dfs(arr, idx+1, n, sum+arr[idx], list);
+		dfs(arr, idx+1, n, sum, list);
+		
 	}
-	
-	private static void makeSum(int sum, int start, int end, ArrayList<Integer> list) {
-		// 지정된 범위까지 재귀 호출이 끝나는 시점에 list에 넣어 모든 부분수열 합의 경우를 list에 담음  
-		if(start == end) {
-			list.add(sum);
-			return;
-		}
-		makeSum(sum, start+1, end, list);
-		makeSum(sum+arr[start], start+1, end, list);
-	}
-
 }
