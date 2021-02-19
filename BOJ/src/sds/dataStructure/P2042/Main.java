a96da4347e1b5a3672c49aed0e794be68fa7d43e
socialDe <jaeuk9407@gmail.com>
2021-02-19 10:50:16 +0900

    Update P2042

--- BOJ/src/sds/dataStructure/P2042/Main.java
+++ BOJ/src/sds/dataStructure/P2042/Main.java
@@ -6,7 +6,7 @@ import java.util.StringTokenizer;
 
 public class Main {
 	static int N, M, K;
-	static int nums[];
+	static long nums[];
 	static long tree[];
 
 	public static void main(String[] args) throws Exception {
@@ -17,18 +17,18 @@ public class Main {
 		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 		
-		nums = new int[N+1];
-		tree = new long[N+1];
+		nums = new long[N + 1];
+		tree = new long[N + 1];
 		
 		for(int i = 1; i <= N; i++) {
-			nums[i] = Integer.parseInt(br.readLine());
+			nums[i] = Long.parseLong(br.readLine());
 			update(i, nums[i]);
 		}
 		for(int i = 0; i < M+K; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
+			long c = Long.parseLong(st.nextToken());
 			
 			// update 연산인 경우 
 			if(a == 1) {
@@ -44,10 +44,10 @@ public class Main {
 	}
 	
 	// i번째 수까지의 누적 합을 계산하는 함수
-	private static long prefix_sum(int index) {
+	private static long prefix_sum(long index) {
 		long result = 0;
 		while(index > 0) {
-			result += tree[index];
+			result += tree[(int)index];
 			// 0이 아닌 마지막 비트만큼 빼가면서 이동
 			index -= (index & -index);
 		}
@@ -55,7 +55,7 @@ public class Main {
 	}
 	
 	// i번째 수를 dif만큼 더하는 함수
-	private static void update(int index, int dif) {
+	private static void update(int index, long dif) {
 		while(index <= N) {
 			tree[index] += dif;
 			index += (index & -index);
@@ -63,7 +63,7 @@ public class Main {
 	}
 	
 	// start부터 end까지의 구간 합을 계산하는 함수
-	private static long interval_sum(int start, int end) {
+	private static long interval_sum(long start, long end) {
 		return prefix_sum(end) - prefix_sum(start-1);
 	}
 	
